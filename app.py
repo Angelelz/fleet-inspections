bde0ac3dc8ef1894229f7de5fb55804ad7953d22
Angelelz <34462576+Angelelz@users.noreply.github.com>
2022-01-25 05:43:57 +0000

    Tue, Jan 25, 2022, 5:43 AM +00:00

--- app.py
+++ app.py
@@ -672,6 +672,7 @@ def vehicles():
             # Fancy way of creating a dictionary with the vehicles as keys and a list of inspections as values
             v = {ve["number"]:[[i["next_oil"], i["miles"], i["date"]] for i in inspections if i["v_id"] == ve["v_id"]] for ve in vehicles}
 
+            # Append 
             for vehicle, i in v.items():
                 miles_oil = 0
                 miles = []
