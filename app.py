017eae44317dcf0b10eb121b52a84a1719fc0537
Angelelz <34462576+Angelelz@users.noreply.github.com>
2022-01-25 06:35:17 +0000

    Tue, Jan 25, 2022, 6:35 AM +00:00

--- app.py
+++ app.py
@@ -666,7 +666,7 @@ def vehicles():
             db = sqlite3.connect(db_path)
             db.row_factory = sqlite3.Row
             vehicles = as_dict(db.execute("SELECT * FROM vehicles WHERE c_id = ? ORDER BY (number + 0)", [session.get("c_id")]).fetchall())
-            inspections = as_dict(db.execute("SELECT * FROM inspections WHERE c_id = ? ORDER BY date DESC", [session.get("c_id")]).fetchall())
+            inspections = as_dict(db.execute("SELECT * FROM inspections WHERE c_id = ? ORDER BY date DESC, i_id DESC LIMIT ?", [session.get("c_id"), MAX_INSPECTIONS]).fetchall())
             db.close()
 
             # Fancy way of creating a dictionary with the vehicles as keys and a list of inspections as values
