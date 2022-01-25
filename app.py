c5a6d36b48154e3d9f80678fdcbb7edd7e2ac4a1
Angelelz <34462576+Angelelz@users.noreply.github.com>
2022-01-25 04:43:33 +0000

    Tue, Jan 25, 2022, 4:43 AM +00:00

--- app.py
+++ app.py
@@ -643,6 +643,7 @@ def vehicles():
                 if i[c[0]] == 0:
                     if c_index != 0:
                         row.append(row[c_index - 1])
+                    print(row)
                     row[c_index][0] = i[c[1]]
                     row[c_index][1] = c[3]
                     c_index += 1
