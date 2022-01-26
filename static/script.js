f48d338249323fe7b6c6e7cb341fb4d85fd9ebe7
Angelelz <34462576+Angelelz@users.noreply.github.com>
2022-01-26 04:01:19 +0000

    Wed, Jan 26, 2022, 4:01 AM +00:00

--- static/script.js
+++ static/script.js
@@ -1,3 +1,40 @@
 switch (window.location.pathname) {
-    
+    case "/": {
+        window.onload = function() {
+            let root = document.documentElement;
+            var table = document.getElementById('index-users');
+            if (table) {
+                var originalWidth = table.style.width;
+                table.style.width = '1px';
+                const smallestWidth = table.getBoundingClientRect().width + 24;
+                table.style.width = originalWidth;
+                smalestWidthText = "(max-width: " + Math.floor(smallestWidth) + "px)";
+
+                var x = window.matchMedia(smalestWidthText);
+
+                function handleTabletChange(e) {
+                    // Check if the media query is true
+                    if (e.matches) {
+                        var tds = document.getElementsByClassName("email-tohide");
+
+                        for (var i = 0; i < tds.length; ++i) {
+                            tds[i].style.display = "none";
+                        }
+                        document.getElementById("head-to-hide").style.display = "none";
+                    }
+                    else {
+                        var tds = document.getElementsByClassName("email-tohide");
+                        for (var i = 0; i < tds.length; ++i) {
+                            tds[i].style.display = "table-cell";
+                        }
+                        document.getElementById("head-to-hide").style.display = "table-cell";
+                    }
+                }
+
+                x.addListener(handleTabletChange);
+                handleTabletChange(x);
+            }
+
+        }
+    }
 }
