a0e6f07bd8927f7cac54bf95c656203807f996d7
Angelelz <34462576+Angelelz@users.noreply.github.com>
2022-01-26 00:58:01 +0000

    Wed, Jan 26, 2022, 12:58 AM +00:00

--- helpers.py
+++ helpers.py
@@ -6,9 +6,8 @@ from functools import wraps
 
 
 def apology(message, code=400):
-    
     """Render message as an apology to user."""
-    '''def escape(s):
+    def escape(s):
         """
         Escape special characters.
 
@@ -18,7 +17,7 @@ def apology(message, code=400):
                          ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
             s = s.replace(old, new)
         return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code'''
+    return render_template("apology.html", top=code, bottom=escape(message)), code
 
 
 def login_required(f):
